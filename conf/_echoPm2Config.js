eeef163edf3e3dd92a022f82b72f3a6eff438a5a
gaomingfei <gaomingfei@jd.com>
2017-08-18 14:53:13 +0800

    [bugfix] process failed when npm start failed

---
+++ conf/_echoPm2Config.js
@@ -0,0 +1,15 @@
+const app_name = 'timeline-' + process.env.ENV
+let instances = process.env.ENV === 'prod' ? 16 : 2
+module.exports = JSON.stringify({
+  'apps': [
+    {
+      'name': app_name,
+      'script': 'bin/www',
+      'log_file': `~/log/${app_name}.log`,
+      'error_file': `~/log/${app_name}-err.log`,
+      'out_file': `~/log/${app_name}-out.log`,
+      'exec_mode': 'cluster',
+      'instances': instances,
+    },
+  ],
+})
--- conf/timeline.js
+++ conf/timeline.js
@@ -1,51 +1,31 @@
-// mongodb
-const MONGODB_ADDRESS = require('./_getMongoConfig')('dbp')
-const MONGODB_COLLECTION = 'timelineJobs'
-// npm dir
-const NPM_DIR = process.env.HOME + '/npm'
-// scan dir schedule
-const SCAN_DIR_SCHEDULE = 30000
-// default name
-const NAME = 'Timeline-' + process.env.ENV + ' ' + process.pid // 当前进程的名字
-const CONFIG_NAME = 'timeline.json'     // 配置文件的名字
-// default option
-const TRANSFER_DELAY = 'in 10 seconds'  // 进程结束后转移本机任务， 指定下次执行任务的时间
-const MAX_CONCURRENCY = 1               // 所有timeline进程所能执行的任务数量
-const LOCK_LIMIT = 1                    // 同一时间内任务队列中最大数量
-const DEFAULT_CONCURRENCY = 1           // 单个任务在一个时间内同时执行的数量
-const DEFAULT_LOCK_LIMIT = 0            // 当前锁定任务的最大数量， 0代表无限
-const DEFAULT_LOCK_LIFETIME = 1200000   // 20 mins
-const LOCAL_TIMEZONE = 'Asia/Shanghai'
-const DEFAULT_DEFINE_OPTION = {
-  priority: 'normal',                   // 当前任务优先级都为normal
-}
-const DEFAULT_EXECUTE_OPTION = {
-  retries: 2,                           // 任务出错重试次数
-  factor: 2,                            // 重试时间间隔指数上涨为2
-  minTimeout: 3000,                     // 最小重试时间间隔
-  maxTimeout: 10000,                    // 最大重试时间间隔
-}
-
-
+// timeline configs
 module.exports = {
-  // timeline config:
-  name: NAME,
+  name: 'Timeline-' + process.env.ENV + ' ' + process.pid,
+  // mongodb
   db: {
-    address: MONGODB_ADDRESS,
-    collection: MONGODB_COLLECTION,
+    address: require('./_getMongoConfig')('dbp'),
+    collection: 'timelineJobs',
   },
-  npm_dir: NPM_DIR,
-  scan_dir_schedule: SCAN_DIR_SCHEDULE,
-  transfer_delay: TRANSFER_DELAY,
-  maxConcurrency: MAX_CONCURRENCY,
-  lockLimit: LOCK_LIMIT,
-  timezone: LOCAL_TIMEZONE,
-  defaultConcurrency: DEFAULT_CONCURRENCY,
-  defaultLockLimit: DEFAULT_LOCK_LIMIT,
-  defaultLockLifetime: DEFAULT_LOCK_LIFETIME,
-
+  // npm dir
+  npm_dir: process.env.HOME + '/npm',
+  config_name: 'timeline.json',     // 配置文件的名字,
+  // default options
+  scan_dir_schedule: 30000,                       // 扫描目录的时间间隔
+  transfer_delay: 'in 10 seconds',                // 进程结束后转移本机任务， 指定下次执行任务的时间
+  maxConcurrency: 1,                              // 所有timeline进程所能执行的任务数量
+  lockLimit: 1,                                   // 同一时间内任务队列中最大数量
+  timezone: 'Asia/Shanghai',                      // 定时任务所用到的时区
+  defaultConcurrency: 1,                          // 单个任务在一个时间内同时执行的数量
+  defaultLockLimit: 0,                            // 当前锁定任务的最大数量， 0代表无限
+  defaultLockLifetime: 1200000,                   // 20 mins
   // job config: 
-  config_name: CONFIG_NAME,
-  default_define_option: DEFAULT_DEFINE_OPTION,
-  default_execute_option: DEFAULT_EXECUTE_OPTION,
+  default_define_option: {
+    priority: 'normal',                           // 当前任务优先级都为normal
+  },
+  default_execute_option: {
+    retries: 2,                                   // 任务出错重试次数
+    factor: 2,                                    // 重试时间间隔指数上涨为2
+    minTimeout: 3000,                             // 最小重试时间间隔
+    maxTimeout: 10000,                            // 最大重试时间间隔
+  },
 }
